80ed360095f7d12b9d6b8e77ed0fc997dce54671
Kamlesh Kumar <patelkamleshpatel364@gmail.com>
2023-12-26 21:51:19 -0800

    update crisp key

--- components/crisp-chat.tsx
+++ components/crisp-chat.tsx
@@ -5,7 +5,7 @@ import { Crisp } from "crisp-sdk-web";
 
 export const CrispChat = () => {
   useEffect(() => {
-    Crisp.configure("8254f25a-eca0-421e-b4de-2bf655c03887");
+    Crisp.configure("1113368c-cc3d-436a-8ae6-e20040fd7f7a");
   }, []);
 
   return null;
